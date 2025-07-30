04f98037693135428b45089d4ca9f065ff301c2e
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-07-30 13:15:33 -0700

    added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,28 @@
-export default function Home(){
-  return<div>
-    <div>
-      <p>hello</p>
+import { useState } from "react";
+
+export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+  const [display, setDisplay] = useState([])
+  function handleButton(){
+  setDisplay((prev)=> ([...prev, inputValue]))
+   setInputValue('')
+  }
+  return (
+    <div className="container">
+      <div className="content">
+        <input
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+
+        <button onClick={handleButton}>Search</button>
+      </div>
+
+      <div>
+        {display.map((value)=>{
+          return <p>{value}</p>
+        })}
+      </div>
     </div>
-  </div>
+  );
 }
