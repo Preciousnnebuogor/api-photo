26058b4bd2602540ef6c79c8dd3536f55f95e25c
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-07-31 17:56:18 -0700

    added auth key

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,10 +2,20 @@ import { useState } from "react";
 
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
-  const [display, setDisplay] = useState([])
-  function handleButton(){
-  setDisplay((prev)=> ([...prev, inputValue]))
-   setInputValue('')
+  const [display, setDisplay] = useState([]);
+
+  const apiKey = process.env.REACT_APP_UNSPLASH_KEY;
+  async function handleSubmit() {
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/search/photos?query=${inputValue}&client_id=${apiKey}`
+      );
+      console.log("Splash Data", response);
+      const data = await response.json();
+      setDisplay(data.results);
+    } catch (err) {
+      console.error("Error fetching images:", err);
+    }
   }
   return (
     <div className="container">
@@ -15,13 +25,22 @@ export default function Home() {
           value={inputValue}
         />
 
-        <button onClick={handleButton}>Search</button>
+        <button onClick={handleSubmit}>Search1</button>
       </div>
 
       <div>
-        {display.map((value)=>{
-          return <p>{value}</p>
-        })}
+        {display &&
+          display.map((items, index) => (
+            <div key={index}>
+              <p>{items.id}</p>
+              {items.alt_description && <p>{items.alt_description}</p>}
+              <img
+                src={items.urls?.small}
+                alt={items.alt_description || "Image"}
+                style={{ width: "200px", height: "auto", marginBottom: "1rem" }}
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
